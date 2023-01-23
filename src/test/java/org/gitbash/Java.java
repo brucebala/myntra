62a501c93a37cc146b1468ec5372f1540263ec58
brucebala <bala10832@gmail.com>
2023-01-23 15:11:51 -0800

    update to new class

---
+++ src/test/java/org/gitbash/Java.java
@@ -0,0 +1,20 @@
+package org.gitbash;
+
+public class Java {
+
+	private void mc1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void mc2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void mc3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+}
