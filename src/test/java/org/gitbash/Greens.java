3e469172b5c95be43ba04de7fad6b2ad9c5ac021
brucebala <bala10832@gmail.com>
2023-01-23 14:50:53 -0800

    create a new class to update in praveenn

---
+++ src/test/java/org/gitbash/Greens.java
@@ -0,0 +1,19 @@
+package org.gitbash;
+
+public class Greens {
+
+	
+	private void siva() {
+		// TODO Auto-generated method stub
+
+	}
+	private void bala() {
+		// TODO Auto-generated method stub
+
+	}
+	private void praveen() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
